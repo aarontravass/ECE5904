2c166f4673851268d31206e92f9079f73432e11f
aaron <aaron.travass@hotmail.com>
2023-04-06 13:34:34 -0400

    added tests

--- threaded_main_convergence.py
+++ threaded_main_convergence.py
@@ -9,18 +9,13 @@ from chess_game.config import BOARD_SCORES
 from time import perf_counter
 from random import randint, choice as choicefn
 
-m2_avg=0
-mcts_avg=0
-m2=0
-mcts=0
+
 random_time = False
 def main():
     # Use a breakpoint in the code line below to debug your script.
     board = Board()
     choice = True;
     start = True
-    global m2, mcts, m2_avg, mcts_avg
-    counter = 0
     while(1):
         if(choice):
             move = None
@@ -33,7 +28,6 @@ def main():
             #print("Player Move", move)
             board.push(Move.from_uci(move))
         else:
-            counter+=1
             pool = Pool(3)
             #print("Bot move")
             cut_of_time = randint(0, 30) if random_time else -1
@@ -56,11 +50,12 @@ def main():
                 moves = [moves.pop()]
 
             best_move = moves
-            
-            print("m2 mcts m4")
+           
+            print(moves)
             if not random_time:
-                m2+=moves[0][1]
-                mcts+= moves[1][1]
+                 if moves[0][0]==moves[1][0]:
+                    print(board.fen())
+                    break
             else:
                 print(cut_of_time, best_move[0][0], best_move[0][1])
             board.push(Move.from_uci(best_move[1][0]))
@@ -74,11 +69,6 @@ def main():
     else:
         result = int(check_win(board, True))
     print(result)
-    m2/=float(counter)
-    mcts/=float(counter)
-    print(m2, mcts)
-    m2_avg += m2
-    mcts_avg += mcts
 
 def mcts_main(board: Board, cut_of_time: int) -> None:
     temp = board.copy()
@@ -91,10 +81,10 @@ def mcts_main(board: Board, cut_of_time: int) -> None:
     
 
 if __name__ == '__main__':
-    itr = 10
+    itr = 1
     for i in range(itr):
         print("starting itr ", i+1)
         main()
         print("completed itr ", i+1)
-    print(m2_avg/float(itr), mcts_avg/float(itr))
+    
 
